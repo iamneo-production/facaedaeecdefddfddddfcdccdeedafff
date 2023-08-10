d64d54fdd7188bb408eaf1e1f9d085b64611a71c
Iamneo <techadmin@iamneo.ai>
2023-08-10 09:32:52 +0000

    Backup Commit

--- jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
+++ jspservelt-app-friends-calculator/src/main/java/controller/MainController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns= {"/jspservelt-app-friends-calculator/friends"})
+@WebServlet(urlPatterns= {"/jspservlet-app-friends-calculator/friends"})
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
